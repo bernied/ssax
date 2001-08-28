08ed9078dad69db20410415d78644ae7c396a7c2
Oleg Kiselyov <oleg@okmij.org>
2001-08-28 04:27:26 +0000

    Imported into CVS for the first time

---
+++ SSAX/examples/run-outline.scm
@@ -0,0 +1,37 @@
+;
+; $Id$
+
+(define docstrings
+ '(
+ " Pretty-print the structure of an XML document, disregarding the "
+ " character data."
+ " This example corresponds to outline.c of the Expat distribution"
+ ""
+ " Usage"
+ "	outline xml-file-name"
+))
+
+(define (parser-error port message . specialising-msgs)
+  (apply cerr (cons message specialising-msgs))
+  (cerr nl)
+  (exit 4))
+(define (SSAX:warn port message . specialising-msgs)
+  (apply cerr (cons message specialising-msgs))
+  (cerr nl))
+
+
+(define (main argv)
+
+  (define (help)
+    (for-each
+     (lambda (docstring) (cerr docstring nl))
+     docstrings)
+    (exit 4))
+
+  (if (not (= 2 (length argv)))
+      (help))		; at least one argument, besides argv[0], is expected
+  (display 
+   (call-with-input-file (cadr argv)
+     outline))
+)
+
