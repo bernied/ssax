cd2b844b3d236d14ba58be7e152930a1adf5495e
Oleg Kiselyov <oleg@okmij.org>
2001-10-08 20:18:27 +0000

    Added functions foldts and replace-range.

--- SSAX/lib/SXML-tree-trans.scm
+++ SSAX/lib/SXML-tree-trans.scm
@@ -3,14 +3,20 @@
 ;
 ; $Id$
 
-		; Output the 'fragments'
-		; The fragments are a list of strings, characters,
-		; numbers, thunks, #f -- and other fragments.
-		; The function traverses the tree depth-first, writes out
-		; strings and characters, executes thunks, and ignores
-		; #f and '().
-		; The function returns #t if anything was written at all;
-		; otherwise the result is #f
+; The following macro runs built-in test cases -- or does not run,
+; depending on which of the two lines below you commented out
+;(define-macro (run-test . body) `(begin (display "\n-->Test\n") ,@body))
+(define-macro (run-test . body) '(begin #f))
+
+; Output the 'fragments'
+; The fragments are a list of strings, characters,
+; numbers, thunks, #f -- and other fragments.
+; The function traverses the tree depth-first, writes out
+; strings and characters, executes thunks, and ignores
+; #f and '().
+; The function returns #t if anything was written at all;
+; otherwise the result is #f
+
 (define (SRV:send-reply . fragments)
   (let loop ((fragments fragments) (result #f))
     (cond
@@ -172,3 +178,194 @@
 	(error "Unknown binding for " trigger " and no default"))))))
 )
 
+;------------------------------------------------------------------------
+;			Extended tree fold
+; tree = atom | (node-name tree ...)
+;
+; foldts fdown fup fhere seed (Leaf str) = fhere seed str
+; foldts fdown fup fhere seed (Nd kids) =
+;         fup seed $ foldl (foldts fdown fup fhere) (fdown seed) kids
+
+; procedure fhere: seed -> atom -> seed
+; procedure fdown: seed -> node -> seed
+; procedure fup: parent-seed -> last-kid-seed -> node -> seed
+; foldts returns the final seed
+
+(define (foldts fdown fup fhere seed tree)
+  (cond
+   ((null? tree) seed)
+   ((not (pair? tree))		; An atom
+    (fhere seed tree))
+   (else
+    (let loop ((kid-seed (fdown seed tree)) (kids (cdr tree)))
+      (if (null? kids)
+	  (fup seed kid-seed tree)
+	  (loop (foldts fdown fup fhere kid-seed (car kids))
+		(cdr kids)))))))
+
+;------------------------------------------------------------------------
+; Traverse a forest depth-first and cut/replace ranges of nodes.
+;
+; The nodes that define a range don't have to have the same immediate
+; parent, don't have to be on the same level, and the end node of a
+; range doesn't even have to exist. A replace-range procedure removes
+; nodes from the beginning node of the range up to (but not including)
+; the end node of the range.  In addition, the beginning node of the
+; range can be replaced by a node or a list of nodes. The range of
+; nodes is cut while depth-first traversing the forest. If all
+; branches of the node are cut a node is cut as well.  The procedure
+; can cut several non-overlapping ranges from a forest.
+
+;	replace-range:: BEG-PRED x END-PRED x FOREST -> FOREST
+; where
+;	type FOREST = (NODE ...)
+;	type NODE = Atom | (Name . FOREST) | FOREST
+;
+; The range of nodes is specified by two predicates, beg-pred and end-pred.
+;	beg-pred:: NODE -> #f | FOREST
+;	end-pred:: NODE -> #f | FOREST
+; The beg-pred predicate decides on the beginning of the range. The node
+; for which the predicate yields non-#f marks the beginning of the range
+; The non-#f value of the predicate replaces the node. The value can be a
+; list of nodes. The replace-range procedure then traverses the tree and skips
+; all the nodes, until the end-pred yields non-#f. The value of the end-pred
+; replaces the end-range node. The new end node and its brothers will be
+; re-scanned.
+; The predicates are evaluated pre-order. We do not descend into a node that
+; is marked as the beginning of the range.
+
+(define (replace-range beg-pred end-pred forest)
+
+  ; loop forest keep? new-forest
+  ; forest is the forest to traverse
+  ; new-forest accumulates the nodes we will keep, in the reverse
+  ; order
+  ; If keep? is #t, keep the curr node if atomic. If the node is not atomic,
+  ; traverse its children and keep those that are not in the skip range.
+  ; If keep? is #f, skip the current node if atomic. Otherwise,
+  ; traverse its children. If all children are skipped, skip the node
+  ; as well.
+
+  (define (loop forest keep? new-forest)
+    (if (null? forest) (values (reverse new-forest) keep?)
+	(let ((node (car forest)))
+	  (if keep?
+	      (cond			; accumulate mode
+	       ((beg-pred node) =>	; see if the node starts the skip range
+		(lambda (repl-branches)	; if so, skip/replace the node
+		  (loop (cdr forest) #f 
+			(append (reverse repl-branches) new-forest))))
+	       ((not (pair? node))	; it's an atom, keep it
+		(loop (cdr forest) keep? (cons node new-forest)))
+	       (else
+		(let-values* 
+		 ((node? (symbol? (car node)))  ; or is it a nodeset?
+		  ((new-kids keep?)		; traverse its children
+		   (loop (if node? (cdr node) node) #t '())))
+		 (loop (cdr forest) keep?
+		       (cons 
+			(if node? (cons (car node) new-kids) new-kids)
+			new-forest)))))
+	      ; skip mode
+	      (cond
+	       ((end-pred node) =>	; end the skip range
+		(lambda (repl-branches)	; repl-branches will be re-scanned
+		  (loop (append repl-branches (cdr forest)) #t
+			new-forest)))
+	       ((not (pair? node))	; it's an atom, skip it
+		(loop (cdr forest) keep? new-forest))
+	       (else
+		(let-values* 
+		 ((node? (symbol? (car node)))  ; or is it a nodeset?
+		  ((new-kids keep?)		; traverse its children
+		   (loop (if node? (cdr node) node) #f '())))
+		 (loop (cdr forest) keep?
+		       (if (or keep? (pair? new-kids))
+			   (cons
+			    (if node? (cons (car node) new-kids) new-kids)
+			    new-forest)
+			   new-forest)		; if all kids are skipped
+		       ))))))))			; skip the node too
+  
+  (let-values* (((new-forest keep?) (loop forest #t '())))
+     new-forest))
+
+; A few tests
+(run-test
+ (let* ((tree
+	 '(root
+	      (n1 (n11) "s12" (n13))
+	    "s2"
+	    (n2 (n21) "s22")
+	    (n3 
+	     (n31 (n311))
+	     "s32"
+	     (n33 (n331) "s332" (n333))
+	     "s34")))
+	(test
+	 (lambda (pred-begin pred-end expected)
+	   (let ((computed
+		  (car (replace-range pred-begin pred-end (list tree)))))
+	     (assert (equal? computed expected)))))
+	)
+   (pp tree)
+      ; Remove one node, "s2"
+   (test
+    (lambda (node)
+      (and (equal? node "s2") '()))
+    (lambda (node) (list node))
+    '(root (n1 (n11) "s12" (n13))
+       (n2 (n21) "s22")
+       (n3 (n31 (n311)) "s32" (n33 (n331) "s332" (n333)) "s34")))
+
+      ; Replace one node, "s2" with "s2-new"
+   (test 
+    (lambda (node)
+      (and (equal? node "s2") '("s2-new")))
+    (lambda (node) (list node))
+    '(root (n1 (n11) "s12" (n13))
+       "s2-new"
+       (n2 (n21) "s22")
+       (n3 (n31 (n311)) "s32" (n33 (n331) "s332" (n333)) "s34")))
+
+      ; Replace one node, "s2" with "s2-new" and its brother (n-new "s")
+   (test 
+    (lambda (node)
+      (and (equal? node "s2") '("s2-new" (n-new "s"))))
+    (lambda (node) (list node))
+    '(root (n1 (n11) "s12" (n13))
+       "s2-new" (n-new "s")
+       (n2 (n21) "s22")
+       (n3 (n31 (n311)) "s32" (n33 (n331) "s332" (n333)) "s34")))
+
+      ; Remove everything from "s2" onward
+   (test 
+    (lambda (node)
+      (and (equal? node "s2") '()))
+    (lambda (node) #f)
+    '(root (n1 (n11) "s12" (n13))))
+   
+      ; Remove everything from "n1" onward
+   (test 
+    (lambda (node)
+      (and (pair? node) (eq? 'n1 (car node)) '()))
+    (lambda (node) #f)
+    '(root))
+
+      ; Replace from n1 through n33
+   (test 
+    (lambda (node)
+      (and (pair? node)
+	   (eq? 'n1 (car node))
+	   (list node '(n1* "s12*"))))
+    (lambda (node)
+      (and (pair? node)
+	   (eq? 'n33 (car node))
+	   (list node)))
+    '(root
+	 (n1 (n11) "s12" (n13))
+       (n1* "s12*")
+       (n3 
+	(n33 (n331) "s332" (n333))
+	"s34")))
+   ))
