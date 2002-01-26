fd46a0291733ce0c6af30edc9207d760ac06e060
Oleg Kiselyov <oleg@okmij.org>
2002-01-26 01:33:13 +0000

    Added the list of supporting Scheme system and a reference to a PADL02 talk about the derivation of the SSAX API.

--- SSAX/ssax-sourceforge.scm
+++ SSAX/ssax-sourceforge.scm
@@ -7,10 +7,10 @@
    (title "XML and Scheme")
    (description "Representing, authoring, querying and transforming
 markup data in Scheme; XML notation for a programming language")
-   (Date-Revision-yyyymmdd "20011012")
+   (Date-Revision-yyyymmdd "20020125")
    (Date-Creation-yyyymmdd "20010706")
    (keywords "XML, XML parsing, XML Infoset, XPath, SAX, SXML, XSLT, Scheme, HTML composition, HTML authoring")
-   (AuthorAddress "oleg@pobox.com")
+   (AuthorAddress "oleg@okmij.org")
    (long-title "SSAX and SXML at SourceForge")
    )
 
@@ -28,7 +28,7 @@ parser, a SAX parser, and a supporting library of lexing and parsing
 procedures. The procedures in the package can be used separately to
 tokenize or parse various pieces of XML documents. The framework
 supports XML Namespaces, character, internal and external parsed
-entities, xml:space, attribute value normalization, processing
+entities, attribute value normalization, processing
 instructions and CDATA sections.  The package includes a
 semi-validating " (em "SXML parser") ": a DOM-mode parser that is an
 instantiation of a SAX parser (called SSAX).")
@@ -48,53 +48,79 @@ of implementing the parsing engine as an enhanced tree fold
 combinator, which fully captures the control pattern of the
 depth-first tree traversal.")
 
-
    (p "Related to SSAX are SXPath queries and SXML transformations.")
 
+   (Section 3 "Availability")
+   (p "The current version of SSAX is 4.9. The whole SSAX code is in public domain.")
+   
+   "SSAX has been tested on the following Scheme systems:"
+   (br)
+   "PLT Scheme (versions 103 and 200), Bigloo 2.4b, GambitC 3.0,
+Chicken, Guile, SCM 5d2, MIT Scheme 7.5.2, Gauche 0.4.12."
+
+
+   (Section 3 "Documentation")
    (p "Main SSAX/SXML page:"
       (URL "http://pobox.com/~oleg/ftp/Scheme/xml.html"))
 
-   (p "SSAX project summary page at SourceForge"
+   (p
+    "The derivation of the SSAX API and the comparison of SSAX with other
+functional-style XML parsers and with the Expat were the topic of a
+presentation at PADL2002, the 4th International Symposium on Practical
+Aspects of Declarative Languages. The presentation (slides + the
+transcript) is available online:"
+    (URL "http://pobox.com/~oleg/ftp/papers/XML-parsing-talk.ps.gz"))
+
+   (p "SSAX-SXML Mailing list:"
+      (URL "http://lists.sourceforge.net/lists/listinfo/ssax-sxml"))
+
+   (p "SSAX project summary page at SourceForge:"
       (URL "http://sourceforge.net/projects/ssax"))
 
-   (dl
 
-    (dt (a (@ (href "http://sf.net/project/showfiles.php?group_id=30687"))
-	   "SSAX distributions"))
-    (dd "The SSAX distributions for particular Scheme systems (PLT Scheme, Bigloo, Chicken, Guile, etc.) are created by Kirill Lisovsky, whose efforts are greatly appreciated. These distributions are also available from his web site " 
-	(URL "http://pair.com/lisovsky/sxml/ssax/")
-	(p "Kirill's web site has detailed instructions for compiling SSAX on different platforms. In particular, you can compile SSAX into " (em "Java bytecode") ", with a Bigloo compiler."))
+   (Section 3 "SSAX distributions")
+   (p "SSAX download site at SourceForge:"
+      (URL "http://sf.net/project/showfiles.php?group_id=30687"))
 
-    (dt (a (@ (href "http://lists.sourceforge.net/lists/listinfo/ssax-sxml"))
-	   "SSAX-SXML Mailing list") (br) (n_))
+   (p
+    "The SSAX distributions for particular Scheme systems (PLT Scheme, Bigloo, Chicken, Guile, etc.) are created by Kirill Lisovsky, whose efforts are greatly appreciated. These distributions are also available from his web site " 
+      (URL "http://pair.com/lisovsky/sxml/ssax/")
+      (br)
+      "Kirill's web site has detailed instructions for compiling SSAX on different platforms. In particular, you can compile SSAX into " (em "Java bytecode") ", with a Bigloo compiler.")
+   (p
+    (a (@ (href
+	   "http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/gauche/SXML/"))
+       "A Gauche distribution of SSAX") " is maintained by Shiro Kawai.")
 
-    (dt (a (@ (href "http://sourceforge.net/cvs/?group_id=30687"))
-	   "CVS Tree"))
-    (dd "The CVS tree includes the complete SSAX/SXML code, some
-documentation, validation tests, as well as several sample applications."
-	(p "You can "
-	   (a (@ (href "http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/ssax"))
-	      "browse the files in the CVS tree")
-	   " from any web browser."))
+  (Section 3 "SSAX CVS Tree")
+  (p (a (@ (href "http://sourceforge.net/cvs/?group_id=30687"))
+	"The CVS Tree")
+     " includes the complete SSAX/SXML code, some
+documentation, validation tests, as well as several sample applications.")
+  (p "You can "
+     (a (@ (href "http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/ssax"))
+	"browse the files in the CVS tree")
+     " from any web browser.")
 
+;    (dl
 
-    (dt (a (@ (href  "http://sourceforge.net/tracker/?group_id=30687"))
-	   "Trackers"))
-    (dd "You can use a tracker to make a suggestion, to request a
-feature, or to report a problem.")
+;     (dt (a (@ (href  "http://sourceforge.net/tracker/?group_id=30687"))
+; 	   "Trackers"))
+;     (dd "You can use a tracker to make a suggestion, to request a
+; feature, or to report a problem.")
 
-    (dt (a (@ (href "http://sourceforge.net/forum/?group_id=30687"))
-	   "Forums"))
-    (dd "You can browse, search, and post messages related to SSAX and SXML"
-	(br) (n_))
+;     (dt (a (@ (href "http://sourceforge.net/forum/?group_id=30687"))
+; 	   "Forums"))
+;     (dd "You can browse, search, and post messages related to SSAX and SXML"
+; 	(br) (n_))
 
-    (dt (a (@ (href "http://sourceforge.net/docman/?group_id=30687"))
-	   "Doc Manager"))
-    (dt (a (@ (href "http://sourceforge.net/pm/?group_id=30687"))
-	   "Task Manager"))
-    (dt (a (@ (href "http://sourceforge.net/survey/?group_id=30687"))
-	   "Surveys"))
-    )
+;     (dt (a (@ (href "http://sourceforge.net/docman/?group_id=30687"))
+; 	   "Doc Manager"))
+;     (dt (a (@ (href "http://sourceforge.net/pm/?group_id=30687"))
+; 	   "Task Manager"))
+;     (dt (a (@ (href "http://sourceforge.net/survey/?group_id=30687"))
+; 	   "Surveys"))
+;     )
 
    (footer)
 
