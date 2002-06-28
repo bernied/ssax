22bf8dcebe1f7433f396582e400dd2e497214420
Oleg Kiselyov <oleg@okmij.org>
2002-06-28 20:58:18 +0000

    Added SRFI-12 (exceptions handling) and SRFI-2 (and-let* form) for Gambit and Bigloo.

--- SSAX/lib/myenv-bigloo.scm
+++ SSAX/lib/myenv-bigloo.scm
@@ -195,13 +195,12 @@
        (car bindings))))
 
 
-			; assoc-primitives with a default clause
-			; If the search in the assoc list fails, the
-			; default action argument is returned. If this
-			; default action turns out to be a thunk,
-			; the result of its evaluation is returned.
-			; If the default action is not given, an error
-			; is signaled
+; assoc-primitives with a default clause
+;
+; If the search in the assoc list fails, the default action argument
+; is returned. If this default action turns out to be a thunk,
+; the result of its evaluation is returned.
+; If the default action is not given, an error is signaled
 
 (define-macro (assq-def key alist . default-action-arg)
   (let ((default-action
@@ -236,3 +235,93 @@
 (define-macro (env.find key) `(%%env.find ',key))
 (define-macro (env.demand key) `(%%env.demand ',key))
 (define-macro (env.bind key value) `(%%env.bind ',key ,value))
+
+
+; Implementation of HANDLE-EXCEPTIONS of SRFI-12.
+;
+; Syntax: handle-exceptions VAR HANDLE-EXPR EXPR1 EXPR2 ...
+; 
+; Evaluates the body expressions EXPR1, EXPR2, ... in sequence with an
+; exception handler constructed from VAR and HANDLE-EXPR. Assuming no
+; exception is raised, the result(s) of the last body expression
+; is(are) the result(s) of the handle-exceptions expression.
+;
+; The exception handler created by handle-exceptions restores the
+; dynamic context (continuation, exception handler, etc.) of the
+; handle-exceptions expression, and then evaluates HANDLE-EXPR with
+; VAR bound to the value provided to the handler.
+
+; Bigloo's native 'try' form already restores the dynamic
+; environment before running the handler
+
+(define-macro (handle-exceptions var handle-expr body . bodies)
+  (let ((escape (gensym)) (proc (gensym)) (mes (gensym)) (obj (gensym)))
+  `(try 
+    (begin ,body ,@bodies)
+			; If we raised the condition explicitly, the proc
+			; is a pair, whose car is the
+			; argument that was passed to 'abort' or 'signal'.
+			; The cdr part of the pair is the
+			; continuation (for a continuable exception)
+    (lambda (,escape ,proc ,mes ,obj)
+      (,escape			; Continue after 'try'
+       (if (pair? ,proc)	; We've caught the exception thrown
+	   (let ((,var (car ,proc)))   ; by abort or signal
+	     ,handle-expr)
+				; Otherwise, this is a native Bigloo error
+	   (let ((,var
+		  (make-property-condition
+		   'exn		; condition kind required by SRFI-12
+		   'message
+		   (list ,proc ,mes ,obj))))
+	     ,handle-expr))))
+    )))
+
+;      AND-LET* -- an AND with local bindings, a guarded LET* special form
+;
+; AND-LET* (formerly know as LAND*) is a generalized AND: it evaluates
+; a sequence of forms one after another till the first one that yields
+; #f; the non-#f result of a form can be bound to a fresh variable and
+; used in the subsequent forms.
+; It is defined in SRFI-2 <http://srfi.schemers.org/srfi-2/>
+; This macro re-writes the and-let* form into a combination of
+; 'and' and 'let'.
+; See vland.scm for the denotational semantics and
+; extensive validation tests.
+
+(define-macro (and-let* claws . body)
+  (if (null? body)
+      (cond 
+       ((null? claws) #t)		; (and-let* () )
+       ((not (pair? claws)) `(error "claws must be a list" ',claws))
+       ; re-write (and-let* ((claw ... last-claw)) ) into
+       ; (and-let* ((claw ...)) body) with 'body' derived from the last-claw
+       (else
+	(let* ((claws-rev (reverse claws))
+	       (last-claw (car claws-rev))
+	       (first-claws (reverse (cdr claws-rev)))
+	       (new-body
+		(if (pair? last-claw)
+		    (if (null? (cdr last-claw))	; (and-let* (... (exp)) )
+			(car last-claw)
+			(cadr last-claw))	; (and-let* (... (var exp)) )
+		    last-claw			; (and-let* (... var) )
+		    )))
+	  (list 'and-let* first-claws new-body))))
+  ; generic case, 'body' is present
+  (let loop ((claws claws))
+    (cond
+     ((null? claws)			; (and-let* () form ...)
+      (cons 'begin body))
+     ((not (pair? claws))
+      `(error "claws must be a list" ',claws))
+     ((not (pair? (car claws)))		; (and-let* ( var claw ...) body ...)
+      (if (symbol? (car claws))
+	  (list 'and (car claws) (loop (cdr claws)))
+	  `(error "wrong claw:" ',(car claws))))
+     ((null? (cdar claws))		; (and-let* ( (exp) claw... ) body ...)
+      (list 'and (caar claws) (loop (cdr claws))))
+     (else				; (and-let* ((var exp) claw...)body...)
+      (list 'let (list (car claws))
+	    (list 'and (caar claws) (loop (cdr claws)))))))
+))
--- SSAX/lib/myenv.scm
+++ SSAX/lib/myenv.scm
@@ -200,13 +200,12 @@
 
 
 
-			; assoc-primitives with a default clause
-			; If the search in the assoc list fails, the
-			; default action argument is returned. If this
-			; default action turns out to be a thunk,
-			; the result of its evaluation is returned.
-			; If the default action is not given, an error
-			; is signaled
+; assoc-primitives with a default clause
+
+; If the search in the assoc list fails, the default action argument
+; is returned. If this default action turns out to be a thunk,
+; the result of its evaluation is returned.
+; If the default action is not given, an error is signaled
 
 (define-macro (assq-def key alist . default-action-arg)
   (let ((default-action
@@ -242,11 +241,13 @@
 (define-macro (env.demand key) `(%%env.demand ',key))
 (define-macro (env.bind key value) `(%%env.bind ',key ,value))
 
-			; Implementation of SRFI-0
-			; Only feature-identifiers srfi-0 and gambit
-			; assumed predefined
+
+; Implementation of SRFI-0
+; Only feature-identifiers srfi-0 and gambit assumed predefined
+; We also add srfi-12 and srfi-2, which are defined below.
+
 (define-macro (cond-expand . clauses)
-  (define feature-ids '(gambit srfi-0))
+  (define feature-ids '(gambit srfi-0 srfi-2 srfi-12))
   (define (feature-req-satisfies? fr) ; does feature-request satisfies?
     (cond
      ((memq fr feature-ids) #t)
@@ -276,3 +277,83 @@
 	    cmd-or-defs*)
 	   (else (loop (cdr clauses))))))))
 
+
+
+; Implementation of HANDLE-EXCEPTIONS of SRFI-12.
+;
+; Syntax: handle-exceptions VAR HANDLE-EXPR EXPR1 EXPR2 ...
+; 
+; Evaluates the body expressions EXPR1, EXPR2, ... in sequence with an
+; exception handler constructed from VAR and HANDLE-EXPR. Assuming no
+; exception is raised, the result(s) of the last body expression
+; is(are) the result(s) of the handle-exceptions expression.
+;
+; The exception handler created by handle-exceptions restores the
+; dynamic context (continuation, exception handler, etc.) of the
+; handle-exceptions expression, and then evaluates HANDLE-EXPR with
+; VAR bound to the value provided to the handler.
+
+; ##dynamic-let is needed to capture and then restore the dynamic env
+
+(define-macro (handle-exceptions var handle-expr body . bodies)
+  (let ((k (gensym)))
+    `((##dynamic-let '()
+       (lambda ()
+        (call-with-current-continuation
+	 (lambda (,k)
+	   (with-exception-handler
+	    (lambda (,var)		      ; The handler will be executed
+	      (,k (lambda () ,handle-expr)))  ; after ##dynamic-let above exits
+	    (lambda ()
+	      (let ((result (begin ,body ,@bodies))) ; multiple-values are
+		(lambda () result)))))))))))	     ; first class in Gambit
+
+
+;      AND-LET* -- an AND with local bindings, a guarded LET* special form
+;
+; AND-LET* (formerly know as LAND*) is a generalized AND: it evaluates
+; a sequence of forms one after another till the first one that yields
+; #f; the non-#f result of a form can be bound to a fresh variable and
+; used in the subsequent forms.
+; It is defined in SRFI-2 <http://srfi.schemers.org/srfi-2/>
+; This macro re-writes the and-let* form into a combination of
+; 'and' and 'let'.
+; See vland.scm for the denotational semantics and
+; extensive validation tests.
+
+(define-macro (and-let* claws . body)
+  (if (null? body)
+      (cond 
+       ((null? claws) #t)		; (and-let* () )
+       ((not (pair? claws)) `(error "claws must be a list" ',claws))
+       ; re-write (and-let* ((claw ... last-claw)) ) into
+       ; (and-let* ((claw ...)) body) with 'body' derived from the last-claw
+       (else
+	(let* ((claws-rev (reverse claws))
+	       (last-claw (car claws-rev))
+	       (first-claws (reverse (cdr claws-rev)))
+	       (new-body
+		(if (pair? last-claw)
+		    (if (null? (cdr last-claw))	; (and-let* (... (exp)) )
+			(car last-claw)
+			(cadr last-claw))	; (and-let* (... (var exp)) )
+		    last-claw			; (and-let* (... var) )
+		    )))
+	  (list 'and-let* first-claws new-body))))
+  ; generic case, 'body' is present
+  (let loop ((claws claws))
+    (cond
+     ((null? claws)			; (and-let* () form ...)
+      (cons 'begin body))
+     ((not (pair? claws))
+      `(error "claws must be a list" ',claws))
+     ((not (pair? (car claws)))		; (and-let* ( var claw ...) body ...)
+      (if (symbol? (car claws))
+	  (list 'and (car claws) (loop (cdr claws)))
+	  `(error "wrong claw:" ',(car claws))))
+     ((null? (cdar claws))		; (and-let* ( (exp) claw... ) body ...)
+      (list 'and (caar claws) (loop (cdr claws))))
+     (else				; (and-let* ((var exp) claw...)body...)
+      (list 'let (list (car claws))
+	    (list 'and (caar claws) (loop (cdr claws)))))))
+))
