6ddff37476a35724d13fceba1db6f8e498d421b5
Oleg Kiselyov <oleg@okmij.org>
2003-04-25 19:21:57 +0000

    Updated for the different position of newlines. Added tests for *preorder* and *macro* rules.

--- SSAX/tests/vSXML-to-HTML.scm
+++ SSAX/tests/vSXML-to-HTML.scm
@@ -28,8 +28,8 @@
 	      ))
   (write (gen #t))
   (newline)
-  (equal-strs?! '("<p>par1par2par3par4</p>" #\newline) (gen #t))
-  (equal-strs?! '("<p>par1par2</p>" #\newline) (gen #f))
+  (equal-strs?! '(#\newline "<p>par1par2par3par4</p>") (gen #t))
+  (equal-strs?! '(#\newline "<p>par1par2</p>") (gen #f))
   )
 
 
@@ -37,26 +37,32 @@
 		(with-output-to-string
 		  (lambda ()
 		    (SXML->HTML exp))))))
-  (equal-strs?! '("<p>&amp;</p>" #\newline) (gen '(p "&")))
+  (equal-strs?! '(#\newline "<p>&amp;</p>") (gen '(p "&")))
   ;(write (gen '(p (@ (ALIGN "center")) "bad chars:" "<>&\"")))
-  (equal-strs?! '("<p align=\"center\">bad chars:&lt;&gt;&amp;&quot;</p>"
-		  #\newline)
+  (equal-strs?! '(#\newline
+		   "<p align=\"center\">bad chars:&lt;&gt;&amp;&quot;</p>")
 		  (gen '(p (@ (align "center")) "bad chars:" "<>&\"")))
-  (equal-strs?! '("<p align=\"center\" atr=\"&lt;value&gt;\">bad chars:<em>&lt;&gt;&amp;&quot;</em>" #\newline "</p>" #\newline)
+  (equal-strs?! '(#\newline
+		   "<p align=\"center\" atr=\"&lt;value&gt;\">bad chars:"
+		   #\newline "<em>&lt;&gt;&amp;&quot;</em></p>")
 		(gen '(p (@ (align "center") (atr "<value>"))
 			 "bad chars:" (em "<>&\""))))
-  (equal-strs?! '("<p align=\"center\" atr=\"&quot;text&quot;\"><br>"
-		  #\newline "<ul compact><li>item 1</li>"
-		  #\newline "</ul>" #\newline "</p>" #\newline)
+  (equal-strs?! '(#\newline
+		   "<p align=\"center\" atr=\"&quot;text&quot;\">"
+		   #\newline "<br>"
+		   #\newline "<ul compact>"
+		   #\newline "<li>item 1</li></ul></p>")
 		(gen '(p (@ (align "center") (atr "\"text\"")) (br)
 			 (ul (@ (compact)) (li "item " 1)))))
-  (equal-strs?!  '("<p><br>" #\newline "<ul compact><li>item 1</li>"
-		   #\newline "</ul>" #\newline "</p>" #\newline)
+  (equal-strs?!  '(#\newline "<p>"
+		   #\newline "<br>"
+		   #\newline "<ul compact>"
+		   #\newline "<li>item 1</li></ul></p>")
 		 (gen '(p (@) (br) (ul (@ (compact)) (li "item " 1)))))
   (equal-strs?! '("Content-type: text/html" #\newline #\newline
 		  "<HTML><HEAD><TITLE>my title</TITLE></HEAD>"
-		  #\newline "<body bgcolor=\"#ffffff\"><p>par1</p>"
-		  #\newline "</body>" #\newline "</HTML>")
+		  #\newline "<body bgcolor=\"#ffffff\">"
+		  #\newline "<p>par1</p></body></HTML>")
 		(gen 
 		 '(html:begin "my title" 
 			      (body (@ (bgcolor "#ffffff")) (p "par1")))))
@@ -71,21 +77,21 @@
        ,(and (< (+ n 1) max-count)
 	     `(a (@ (href "base-url&slide=" ,(+ n 1))) "next"))
        (p "the text of the slide"))))
-  (equal-strs?! '("<h2>Slide number:0</h2>" #\newline
-		  "<p>the text of the slide</p>" #\newline)
+  (equal-strs?! '(#\newline "<h2>Slide number:0</h2>" 
+		  #\newline "<p>the text of the slide</p>")
 		(with-output-to-string (lambda () (print-slide 0 1))))
-  (equal-strs?! '("<h2>Slide number:0</h2>" #\newline
-		  "<a href=\"base-url&amp;slide=1\">next</a>"
-		  #\newline "<p>the text of the slide</p>" #\newline)
+  (equal-strs?! '(#\newline "<h2>Slide number:0</h2>"
+		  #\newline "<a href=\"base-url&amp;slide=1\">next</a>"
+		  #\newline "<p>the text of the slide</p>")
 		(with-output-to-string (lambda () (print-slide 0 3))))
-  (equal-strs?! '("<h2>Slide number:1</h2>" #\newline
-		  "<a href=\"base-url&amp;slide=0\">prev</a>"
+  (equal-strs?! '(#\newline "<h2>Slide number:1</h2>"
+		  #\newline "<a href=\"base-url&amp;slide=0\">prev</a>"
 		  #\newline "<a href=\"base-url&amp;slide=2\">next</a>"
-		  #\newline "<p>the text of the slide</p>" #\newline)
+		  #\newline "<p>the text of the slide</p>")
 		(with-output-to-string (lambda () (print-slide 1 3))))
-  (equal-strs?! '("<h2>Slide number:2</h2>" #\newline
-		  "<a href=\"base-url&amp;slide=1\">prev</a>"
-		  #\newline "<p>the text of the slide</p>" #\newline)
+  (equal-strs?! '(#\newline "<h2>Slide number:2</h2>"
+		  #\newline "<a href=\"base-url&amp;slide=1\">prev</a>"
+		  #\newline "<p>the text of the slide</p>")
 		(with-output-to-string (lambda () (print-slide 2 3))))
   )
 
@@ -98,5 +104,44 @@
 	    ("slides/slide0010.gif" . "Summary")))
    )
  )
+
+; Testing *preorder* and *macro* rules
+
+(let ()
+  (define (custom-sxml->html tree)
+    (with-output-to-string (lambda () 
+     (SRV:send-reply
+      (pre-post-order tree
+                ; Universal transformation rules. Work for every HTML,
+                ; present and future
+	`((@
+	    ((*default*       ; local override for attributes
+	       . ,(lambda (attr-key . value) (enattr attr-key value))))
+	    . ,(lambda (trigger . value) (cons '@ value)))
+	   (*default* . ,(lambda (tag . elems) (entag tag elems)))
+	   (*text* . ,(lambda (trigger str) 
+			(if (string? str) (string->goodHTML str) str)))
+	  (link
+	   *macro*
+	    . ,(lambda (tag url body)
+		 `(a (@ (href ,url)) ,body)))
+	   (vspace		; (vspace flag)
+	     *preorder*			; where flag is a symbol: small, large
+	     . ,(lambda (tag flag)
+		  (case flag
+		    ((large) (list "<p><br>&nbsp;</p>"))
+		    ((small) (list "<br>&nbsp;<br>"))
+		    (else (error "wrong flag:" flag))))))
+	)))))
+  (equal-strs?! '(#\newline "<p>text" 
+		  #\newline "<a href=\"url\">&lt;body&gt;</a>text1</p>")
+    (custom-sxml->html '(p "text" (link "url" "<body>") "text1")))
+
+  (equal-strs?! '(#\newline "<p>text<br>&nbsp;<br>text1</p>")
+    (custom-sxml->html '(p "text" (vspace small) "text1")))
+
+  (equal-strs?! '(#\newline "<p>text<p><br>&nbsp;</p>text1</p>")
+    (custom-sxml->html '(p "text" (vspace large) "text1")))
+  )
 
 (cout nl nl "All tests passed" nl)
