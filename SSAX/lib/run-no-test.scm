ede6e97f6076d9363c3793b6b332351fdada6385
Oleg Kiselyov <oleg@okmij.org>
2004-12-01 13:56:49 +0000

    This commit was manufactured by cvs2svn to create tag 'modularization_bp'.
    Sprout from master 2004-12-01 13:56:48 UTC Dmitry Lizorkin <dmitry.lizorkin@gmail.com> 'sxml:invert -> sxml:complement for consistency with sxpathlib'
    Delete:
        CVSROOT/checkoutlist
        CVSROOT/commitinfo
        CVSROOT/config
        CVSROOT/cvswrappers
        CVSROOT/editinfo
        CVSROOT/loginfo
        CVSROOT/modules
        CVSROOT/notify
        CVSROOT/rcsinfo
        CVSROOT/taginfo
        CVSROOT/verifymsg
        SSAX/README.dr
        SSAX/benchmarks/Makefile
        SSAX/benchmarks/README
        SSAX/benchmarks/SSAX-benchmark-1.txt
        SSAX/benchmarks/make-file-bench1.scm
        SSAX/benchmarks/read-NCName-ss.scm
        SSAX/benchmarks/rusage.c
        SSAX/benchmarks/string-value-by-one.c
        SSAX/benchmarks/string-value-ssax-ss.scm
        SSAX/benchmarks/string-value-ssax.scm
        SSAX/benchmarks/string-value.c
        SSAX/benchmarks/substring-symbol.c
        SSAX/docs/Makefile
        SSAX/docs/SSAX-benchmark-1.html
        SSAX/docs/SSAX-benchmark-1.scm
        SSAX/docs/SXML-paper.scm
        SSAX/docs/SXML-paper.tex
        SSAX/docs/SXML.html
        SSAX/docs/SXML.scm
        SSAX/docs/SXs.scm
        SSAX/docs/SXs.tex
        SSAX/docs/index.html
        SSAX/docs/parent-pointers.txt
        SSAX/examples/Makefile
        SSAX/examples/README
        SSAX/examples/apply-templates.scm
        SSAX/examples/build-dependencies.scm
        SSAX/examples/daml-parse-unparse.scm
        SSAX/examples/html-parser.scm
        SSAX/examples/outline.scm
        SSAX/examples/packages.scm
        SSAX/examples/parent-pointers.scm
        SSAX/examples/pull-punct-sxml.scm
        SSAX/examples/remove-markup.scm
        SSAX/examples/run-outline-bigloo.scm
        SSAX/examples/run-outline.scm
        SSAX/examples/run-remove-markup-bigloo.scm
        SSAX/examples/run-remove-markup.scm
        SSAX/examples/run-sxml-bigloo.scm
        SSAX/examples/run-sxml.scm
        SSAX/examples/sxml-db-conv.scm
        SSAX/examples/sxml-nesting-depth-label.scm
        SSAX/examples/sxml-to-sxml.scm
        SSAX/examples/sxslt-advanced.scm
        SSAX/examples/target-dependencies
        SSAX/examples/validate-doctype-simple.scm
        SSAX/examples/xml/CongoProfile.daml
        SSAX/examples/xml/OMF-sample.xml
        SSAX/examples/xml/ddn.rdf
        SSAX/examples/xml/total_weather.xsl
        SSAX/lib/Makefile
        SSAX/lib/SSAX-code.scm
        SSAX/lib/SSAX-expanded.scm
        SSAX/lib/SSAX.scm
        SSAX/lib/SXML-to-HTML-ext.scm
        SSAX/lib/SXML-to-HTML.scm
        SSAX/lib/SXML-tree-trans.scm
        SSAX/lib/SXPath-old.scm
        SSAX/lib/assert.scm
        SSAX/lib/catch-error.scm
        SSAX/lib/char-encoding.scm
        SSAX/lib/control.scm
        SSAX/lib/define-opt.scm
        SSAX/lib/env.scm
        SSAX/lib/extract-doc.scm
        SSAX/lib/input-parse.scm
        SSAX/lib/look-for-str.scm
        SSAX/lib/lookup-def.scm
        SSAX/lib/myenv-bigloo.scm
        SSAX/lib/myenv-chez.scm
        SSAX/lib/myenv-mit.scm
        SSAX/lib/myenv-scm.scm
        SSAX/lib/myenv.scm
        SSAX/lib/output.scm
        SSAX/lib/packages.scm
        SSAX/lib/parser-errors-vanilla.scm
        SSAX/lib/ppretty-prints.scm
        SSAX/lib/run-no-test.scm
        SSAX/lib/srfi-12.scm
        SSAX/lib/srfi-13-local.scm
        SSAX/lib/ssax-warn-vanilla.scm
        SSAX/lib/util.scm
        SSAX/ssax-sourceforge.scm
        SSAX/tests/Makefile
        SSAX/tests/vSXML-to-HTML.scm
        SSAX/tests/vSXML-tree-trans.scm
        SSAX/tests/vinput-parse-bigloo.scm
        SSAX/tests/vinput-parse.scm
        SSAX/tests/vland.scm
        SSAX/tests/vmyenv-bigloo.scm
        SSAX/tests/vmyenv.scm
        SSAX/tests/vsrfi-12-bigloo.scm
        SSAX/tests/vsrfi-12.scm
        SSAX/tests/vsrfi-13.scm

--- SSAX/lib/run-no-test.scm
+++
@@ -1,4 +0,0 @@
-; The following macro DOES not run built-in test cases
-(define-macro (run-test . body)
-  '(begin #f))
-
--- SSAX/lib/ssax-warn-vanilla.scm
+++
@@ -1,4 +0,0 @@
-(define (SSAX:warn port msg . other-msg)
-  (apply cerr (cons (string-append (string #\newline) "Warning: ")
-		    (cons msg
-			  other-msg))))
