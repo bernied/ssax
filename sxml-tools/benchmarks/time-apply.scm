b111cc7d24b180bcc3df88aacc610ac63bc6117a
Dmitry Lizorkin <dmitry.lizorkin@gmail.com>
2006-10-30 15:36:51 +0000

    cond-expand branch for Chicken added

--- sxml-tools/benchmarks/time-apply.scm
+++ sxml-tools/benchmarks/time-apply.scm
@@ -12,16 +12,32 @@
 ; Returns:  (cons f-result exec-time)
 ; f-result - result produced by applying procedure `f'
 ; exec-time - time in milliseconds taken by `f' to execute
-(define sxml:time-apply
-  (cond-expand
-   (plt
-    (lambda (f argv)
-      (let-values
-          (((f-res-lst t1 t2 t3) (time-apply f argv)))
-        (cons (car f-res-lst) t2))))
-   (gambit
-    (lambda (f argv)
-      (let ((t-begin (time->seconds (current-time))))
-        (let ((f-res (apply f argv)))
-          (let ((t-end (time->seconds (current-time))))
-            (cons f-res (* (- t-end t-begin) 1000)))))))))
+(cond-expand
+ 
+ (chicken
+  (define (sxml:time-apply f argv)
+    (call-with-values
+     (lambda () (cpu-time))
+     (lambda (user-begin system-begin)
+       (let ((res (apply f argv)))
+         (call-with-values
+          (lambda () (cpu-time))
+          (lambda (user-end system-end)
+            (cons res
+                  (- (+ user-end system-end)
+                     (+ user-begin system-begin))))))))))
+ 
+ (plt
+  (define (sxml:time-apply f argv)
+    (call-with-values
+     (lambda () (time-apply f argv))
+     (lambda (f-res-lst t1 t2 t3)
+       (cons (car f-res-lst) t2)))))
+ 
+ (gambit
+  (define (sxml:time-apply f argv)
+    (let ((t-begin (time->seconds (current-time))))
+      (let ((f-res (apply f argv)))
+        (let ((t-end (time->seconds (current-time))))
+          (cons f-res (* (- t-end t-begin) 1000)))))))
+)
